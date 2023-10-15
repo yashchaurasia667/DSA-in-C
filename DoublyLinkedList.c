c0e6fa9c28f9e33155ba4a39263d2f033ed5317d
Yash <yashchaurasia630@gmail.com>
2023-10-15 16:39:49 +0530

    Added Insertion and Traversal

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -9,9 +9,37 @@ struct Node
 };
 
 //
+//  Traversal
+void traversal(struct Node *head) {
+    struct Node *new = head;
+
+    while(new != NULL) {
+        printf("%d", new->data);
+    }
+}
+
+//
 //  Insertion
 struct Node * insertAt(struct Node *head, int data, int position){
-    
+    struct Node *new, *curr = head;
+    new = (struct Node *) malloc(sizeof(struct Node));
+
+    if(position != 1) {
+        while(position-2) {
+            curr = curr->next;
+            position--;
+        }
+
+        new->next = curr->next;
+        new->prev = curr;
+        curr->next = new;
+        return head;
+    }
+
+    new->prev = NULL;
+    new->next = head;
+    head->prev = new;
+    return new;
 }
 
 int main() {
